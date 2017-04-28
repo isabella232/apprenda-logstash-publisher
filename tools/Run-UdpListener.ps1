c58868fc5eb00825bfbc6cf8d71f857174f56ed3
Jason L. van Brackel <jvanbrackel@apprenda.com>
2017-04-28 16:56:35 -0400

    Make the test non-blocking

--- tools/Run-UdpListener.ps1
+++ tools/Run-UdpListener.ps1
@@ -1,17 +1,45 @@
 [CmdletBinding()]
 param(
-    [int] $port
-) 
+    [int] $port = 10000,
+    [int] $timeout = 10
+)
+process 
 {
-    $address = [System.Net.IPAddress]::Any
-    $endpoint = [System.Net.IPEndPoint]::new($address, $port)
-    $client = [System.Net.Sockets.UdpClient]::new($endpoint)
+    Set-StrictMode -Version Latest
+    $ErrorActionPreference = 'Stop'
 
-    for($i=0; $i -lt 10; i++)
-    {
-        $bytes = $client.Receive([ref]$endpoint)
-        $output = [System.Text.Encoding]::Unicode.GetString($bytes, 0, $bytes.Length)
-        Write-Host $output
-        Start-Sleep -Seconds 1
+     $job = Start-Job {
+                $address = [System.Net.IPAddress]::Any
+                $endPoint = [System.Net.IPEndPoint]::new($address, $args[0])
+                $client = [System.Net.Sockets.UdpClient]::new($endPoint)
+                while($true) {
+                    Write-Host "Listening..."
+                    $bytes = $client.Receive([ref]$endPoint)
+                    $output = [System.Text.Encoding]::Unicode.GetString($bytes, 0, $bytes.Length)
+                    Write-Host $output
+                    Start-Sleep -seconds 2
+                }
+        } -ArgumentList $port
+    
+    $dontStop = $true
+    $loop = 0
+    while($dontStop) {
+        if($job.HasMoreData) {
+            $job | Receive-Job
+        }
+        Start-Sleep -seconds 1
+        $loop++
+
+        if($loop -gt $timeout -and $timeout -ne 0) {
+            $dontStop = $true
+            $job.StopJob()
+        }
     }
+
+    while($job.PSEndTime -eq  $null) {
+        "Waiting receiver job to finish"
+        Start-Sleep -seconds 1
+    }
+
+    $job | Remove-Job
 }
--- tools/Test-LogForwardingService.ps1
+++ tools/Test-LogForwardingService.ps1
@@ -1,67 +1,87 @@
-﻿    [CmdletBinding()]
-    param($configuration = "Debug")
-    $msBuildPath = (Get-MsBuildVersion -Version Latest).GetValue("MSBuildToolsPath")
-    $solutionPath = "./src/Apprenda.ClientServices.LogStashPublisher.sln"
-    $applicationName = "logstashforwarder"
-    $archivePath = "./logstashforwarder.zip"
-    $version = "v1"
+﻿[CmdletBinding()]
+param(
+    $configuration = "Debug",
+    $timeout = 10
+)
 
-    "Checking for MSBuild."
-    if($env:Path -notlike "*$msBuildPath*") {
-        "Adding MSBuild to your path."
-        $env:Path += ";$msBuildPath"
-    } else {
-        "Found MSBuild."
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$msBuildPath = (Get-MsBuildVersion -Version Latest).GetValue("MSBuildToolsPath")
+$solutionPath = "./src/Apprenda.ClientServices.LogStashPublisher.sln"
+$applicationName = "logstashforwarder"
+$archivePath = "./logstashforwarder.zip"
+$version = "v1"
+
+"Checking for MSBuild."
+if($env:Path -notlike "*$msBuildPath*") {
+    "Adding MSBuild to your path."
+    $env:Path += ";$msBuildPath"
+} else {
+    "Found MSBuild."
+}
+
+"Cleaning Solution."
+Clean-Solution $solutionPath
+
+"Building Solution."
+Build-Solution $solutionPath
+
+Package-ApprendaApplication -ArchivePath $archivePath -Services (@{ Name = "LogStashPublisher"; Path ="./src/Publisher/bin/$configuration" })
+
+"Signing into Apprenda"    
+New-ApprendaSession "jvanbrackel@apprenda.com" "password" "JvB" "https://apps.apprenda.jvb"
+
+"Checking for an existing application"
+if(Get-ApprendaApplication $applicationName -ErrorAction SilentlyContinue) {
+    "Removing $applicationName"
+    Remove-ApprendaApplication $applicationName
+}
+
+"Creating $applicationName"
+New-ApprendaApplication $applicationName
+
+"Uploading $archivePath"
+Set-ApprendaApplicationArchive $applicationName $version $archivePath
+
+"Promoting to Published"
+Promote-ApprendaApplication -ApplicationAlias $applicationName -VersionAlias $version -Stage "Published"
+
+"Setting Registry Key"
+New-ApprendaRegistrySetting -Setting "Apprenda.Logging.ExternalServiceAppVersion" -Value "$applicationName(v1)/LogStashForwarderService" -ErrorAction SilentlyContinue
+
+"Turn the log level up to 11"
+Set-ApprendaGlobalLogLevel "Debug"
+
+"Start Listening"
+$loopCount = 0
+$workingDirectory = Get-Location
+$foundOrErrorOrTimeout = $false
+$listenerJob = Start-Job { Set-Location $args[0]; .\tools\Run-UdpListener.ps1 10000 } -ArgumentList $workingDirectory
+while(!($foundOrErrorOrTimeout)) {
+    if($loopCount -ge $timeout)
+    {
+        $foundOrErrorOrTimeout = $true
+    }	
+    $loopCount++
+    if($listenerJob.HasMoreData)
+    {
+        $jobOutput = $listenerJob | Receive-Job
+        Write-Host $jobOutput
     }
+    Start-Sleep -Seconds 1
+}
 
-    "Cleaning Solution."
-    Clean-Solution $solutionPath
+$listenerJob.StopJob()
 
-    "Building Solution."
-    Build-Solution $solutionPath
+while($listenerJob.PSEndTime -eq $null)
+{
+    "Waiting for the listener to stop."
+    Start-Sleep -Seconds 2
+}
 
-    Package-ApprendaApplication -ArchivePath $archivePath -Services (@{ Name = "LogStashPublisher"; Path ="./src/Publisher/bin/$configuration" })
+"Turn the log level back down."
+Set-ApprendaGlobalLogLevel "Error"
 
-    "Signing into Apprenda"    
-    New-ApprendaSession "jvanbrackel@apprenda.com" "password" "JvB" "https://apps.apprenda.jvb"
-
-    "Checking for an existing application"
-    if(Get-ApprendaApplication $applicationName -ErrorAction SilentlyContinue) {
-        "Removing $applicationName"
-        Remove-ApprendaApplication $applicationName
-    }
-
-    "Creating $applicationName"
-    New-ApprendaApplication $applicationName
-
-    "Uploading $archivePath"
-    Set-ApprendaApplicationArchive $applicationName $version $archivePath
-
-    "Promoting to Published"
-    Promote-ApprendaApplication -ApplicationAlias $applicationName -VersionAlias $version -Stage "Published"
-  
-    "Setting Registry Key"
-    New-ApprendaRegistrySetting -Setting "Apprenda.Logging.ExternalServiceAppVersion" -Value "$applicationName(v1)/LogStashForwarderService" -ErrorAction SilentlyContinue
-
-	"Turn the log level up to 11"
-	Set-ApprendaGlobalLogLeve Debug
-
-	"Start Listening"
-	$loopCount = 0
-	$workingDirectory = Get-Location
-	$foundOrErrorOrTimeout = $false
-	$listenerJob = Start-Job { Set-Location $args[0]; .\tools\Run-UdpListener.ps1 10000 } -ArgumentList $workingDirectory
-	while(!($foundOrErrorOrTimeout)) {
-		if($loopCount -ge 2)
-		{
-			$foundOrErrorOrTimeout = $true
-		}	
-		$loopCount++
-		if($listenerJob.HasMoreData)
-		{
-			$jobOutput = $listenerJob | ReceiveJob
-            Write-Host $jobOutput
-            $foundOrErrorOrTimeout = $true
-		}
-        Start-Sleep -Seconds 1
-	}   
+"Cleaning up the listener job."
+$listenerJob | Remove-Job
