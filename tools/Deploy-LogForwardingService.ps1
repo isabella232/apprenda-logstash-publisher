396ef2ed7811cdb81266ca06b9d1a8a30f7512c5
Jason L. van Brackel <jason@vanbrackel.net>
2017-06-01 12:58:42 -0400

    Finish v1 of deployment and promotion scripts

--- tools/Deploy-LogForwardingService.ps1
+++ tools/Deploy-LogForwardingService.ps1
@@ -8,6 +8,9 @@
     $applicationName = "logstashforwarder"
     $version = "v1"
     $archivePath = ".\logstashforwarder.zip"
+    $addOnArchviePath =".\logstashAddOn.zip"
+    $addOnAlias = "logstash"
+    $addOnInstanceAlias = "logstashForwarder"
 
 
     "Signing into Apprenda"    
@@ -25,11 +28,16 @@
     "Uploading $archivePath"
     Set-ApprendaApplicationArchive $applicationName $version $archivePath
 
-    #TODO: Get-ApprendaAddO
-    #TODO: Remove-ApprendaAddOn
-    #TODO: New-ApprendaAddOn
-
-    Write-Warning "Setting Add-On Properties are not current suported by the API.  Please navigate to $($apprendaSession.url)/soc/Configuration/AddOns.aspx and set the logstash add on properties for the host."
-    Write-Warning "See story APPRENDA-22542 for more details."
-    Write-Warning "Once that is complete you can complete this process by running Promote-LogForwardingService, which includes the call New-ApprendaAddOnInstance -Alias `"logstash`" -InstaceAlias `"logstashForwarder`";"
+    "Creating the Apprenda AddOn if needed"
+    $currentAddOn = Get-ApprendaAddOn -Alias $addOnAlias  -ErrorAction SilentlyContinue
+    
+    if($currentAddOn -ne $null) {
+        "Add-on $addOnAlias found."
+    } else {
+        New-ApprendaAddOn -Alias $addOnAlias -Path $addOnArchviePath
+        Write-Warning "Setting Add-On Properties are not current suported by the API.  Please navigate to $($apprendaSession.url)/soc/Configuration/AddOns.aspx and set the logstash add on properties for the host."
+        Write-Warning "See story APPRENDA-22542 for more details."
+        Write-Warning "Once that is complete you can complete this process by running Promote-LogForwardingService, which includes the call New-ApprendaAddOnInstance -Alias `"$addOnAlias`" -InstaceAlias `"$addOnInstanceAlias`";"
+        throw "Stopping execution so the operator can adjust the add on settings in the SOC."
+    }   
 }
--- tools/Promote-LogForwardingService.ps1
+++ tools/Promote-LogForwardingService.ps1
@@ -2,14 +2,23 @@ function Promote-LogForwardingService {
     [CmdletBinding()]
     param()
     process {
+        Set-StrictMode -Version Latest
+        $ErrorActionPreference = 'Stop'
+        
         $addOnAlias = "logstash"
         $addOnInstanceAlias = "logstashForwarder"
         $applicationName = "logstashforwarder"
         $version = "v1"
 
-        #TODO Get and or remove instance
-        "Acquiring a Logstash Add-On Instnace"
-        New-ApprendaAddOnInstance -Alias $addOnAlias -InstanceAlias $addOnInstanceAlias
+        "Acquiring a Logstash Add-On Instnace if needed"
+        $addOnInstance = Get-ApprendaAddOnInstance -Alias $addOnAlias -InstanceAlias $addOnInstanceAlias
+        
+        if($addOnInstance -eq $null) {
+            "No instance found. Creating an instnace of $addOnAlias and naming it $addOnInstanceAlias" 
+            New-ApprendaAddOnInstance -Alias $addOnAlias -InstanceAlias $addOnInstanceAlias        
+        } else {
+            "Instance $addOnInstanceAlias of $addOnAlias found."
+        }
 
         "Promoting to Published"
         Promote-ApprendaApplication -ApplicationAlias $applicationName -VersionAlias $version -Stage "Published"
